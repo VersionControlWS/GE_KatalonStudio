fcf42cbf249cfcc10d949e0ab7a395aee27f28d0
VersionControlWS <versioncontrolws@gmail.com>
2023-02-06 18:09:27 +0530

    main

--- Scripts/Create a new user/Script1571280039907.groovy
+++ Scripts/Create a new user/Script1571280039907.groovy
@@ -13,7 +13,5 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-CustomKeywords.'sample.Common.createNewUser'(age as Integer, username, password, gender, 200)
-
 System.out.println("Working Perfect")
 println("Working Perfect")
